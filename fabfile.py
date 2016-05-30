f25428af49c552aeb175e523a818dbf333e5346d
Woody Huang <woody1573@163.com>
2016-05-30 20:42:05 +0800

    update fab and .gitignore

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import run, env, cd, sudo, prompt, lcd, local
+from fabric.contrib.files import exists
 
 env.hosts = [
     "find_rice_runner@111.206.219.158:22",
@@ -6,25 +7,27 @@ env.hosts = [
 env.password = "shanghai"
 
 
-def get_latest_project_file():
+def deploy():
+    with cd("/home/findRice"):
+        if not exists("./findRice"):
+            print "Project not found, cloning from github: https://github.com/huangy10/findRice.git"
+            run("git clone https://github.com/huangy10/findRice.git")
     with cd("/home/findRice/findRice"):
+        print "Updating source code from github"
         # update remote code
         run("git fetch")
-        run("git merge")
+        run("git pull origin master")
+        print "Applying server settings"
         # replace setting file
-        run("rm findRice/settings.py")
-        run("mv findRice/settings_remote.py findRice/settings.py")
+        sudo("rm findRice/settings.py")
+        sudo("mv findRice/settings_remote.py findRice/settings.py")
         # database migrations
         migrate_db = prompt("Did you modify the database? [Y/n]", validate=r'[yYNn]')
         if migrate_db in ["Y", "y"]:
             run("workon find_rice_env && python manage.py makemigrations")
             run("workon find_rice_env && python manage.py migrate")
         # restart service
-        sudo("supervisorctl restart find_rice")
-        sudo("supervisorctl restart find_rice_celery")
+        print "Restarting server"
+        sudo("supervisorctl restart find_rice find_rice_celery")
 
-
-def test_local():
-    with lcd("~/Project"):
-        local("ls -al")
 
